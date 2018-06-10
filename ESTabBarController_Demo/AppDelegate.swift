859328dd2326da519e4636bab201b0ffd0848084
utmboffice.chen@gmail.com <utmboffice.chen@gmail.com>
2018-06-10 16:04:57 -0400

    done ESTabBarController

--- ESTabBarController_Demo/AppDelegate.swift
+++ ESTabBarController_Demo/AppDelegate.swift
@@ -7,15 +7,71 @@
 //
 
 import UIKit
+import CoreData
+import ESTabBarController_swift
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate,UITabBarControllerDelegate {
 
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        let tabBarController = ESTabBarController()
+        tabBarController.delegate = self
+        tabBarController.title = "Irregularity"
+        tabBarController.tabBar.shadowImage = UIImage(named: "transparent")
+        tabBarController.tabBar.backgroundImage = UIImage(named: "background_dark")
+        tabBarController.shouldHijackHandler = {
+            tabbarController, viewController, index in
+            if index == 2{
+                return true
+            }
+            return false
+        }
+        tabBarController.didHijackHandler = {
+            [weak tabBarController] tabbarController, viewController, index in
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                let alertController = UIAlertController.init(title: nil, message: nil, preferredStyle: .actionSheet)
+                let takePhotoAction = UIAlertAction(title: "Take a photo", style: .default, handler: nil)
+                alertController.addAction(takePhotoAction)
+                let selectFromAlbumAction = UIAlertAction(title: "Select from album", style: .default, handler: nil)
+                alertController.addAction(selectFromAlbumAction)
+                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+                alertController.addAction(cancelAction)
+                tabBarController?.present(alertController, animated: true, completion: nil)
+            }
+        }
+        let v01 = UIViewController()
+        let v02 = UIViewController()
+        let v03 = UIViewController()
+        let v04 = UIViewController()
+        let v05 = UIViewController()
+//        let v1 = UIViewController()
+//        let v2 = UIViewController()
+//        let v3 = UIViewController()
+//        let v4 = UIViewController()
+//        let v5 = UIViewController()
+        let v1 = UINavigationController(rootViewController: v01)
+        let v2 = UINavigationController(rootViewController: v02)
+        let v3 = UINavigationController(rootViewController: v03)
+        let v4 = UINavigationController(rootViewController: v04)
+        let v5 = UINavigationController(rootViewController: v05)
+        v1.tabBarItem = ESTabBarItem.init(ExampleIrregularityBasicContentView(), title: "Home", image: UIImage(named: "home"), selectedImage: UIImage(named: "home_1"))
+        v2.tabBarItem = ESTabBarItem.init(ExampleIrregularityBasicContentView(), title: "Find", image: UIImage(named: "find"), selectedImage: UIImage(named: "find_1"))
+        v3.tabBarItem = ESTabBarItem.init(ExampleIrregularityContentView(), title: nil, image: UIImage(named: "photo_verybig"), selectedImage: UIImage(named: "photo_verybig"))
+        v4.tabBarItem = ESTabBarItem.init(ExampleIrregularityBasicContentView(), title: "Favor", image: UIImage(named: "favor"), selectedImage: UIImage(named: "favor_1"))
+        v5.tabBarItem = ESTabBarItem.init(ExampleIrregularityBasicContentView(), title: "Me", image: UIImage(named: "me"), selectedImage: UIImage(named: "me_1"))
+        
+        tabBarController.viewControllers = [v1, v2, v3, v4, v5]
+        
+//        let navigationController = UINavigationController.init(rootViewController: tabBarController)
+        tabBarController.title = "Demo"
+        self.window?.rootViewController = tabBarController
         return true
     }
 
@@ -39,6 +95,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        
     }
 
 
